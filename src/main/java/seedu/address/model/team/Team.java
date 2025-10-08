ac05112c051e863a747b9f55724b9d9b924cd211
Bacen <ywj.ethan@gmail.com>
2025-10-08 16:57:27 +0800

    Add team and relevant classes and add size() to UniquePersonList

---
+++ src/main/java/seedu/address/model/team/Team.java
@@ -0,0 +1,171 @@
+package seedu.address.model.team;
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+import java.util.List;
+
+import javafx.collections.ObservableList;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.model.ReadOnlyTeam;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.UniquePersonList;
+import seedu.address.model.team.exceptions.TeamMaxCapacityException;
+
+/**
+ * Wraps all data at the team level
+ * Duplicates are not allowed (by .isSamePerson comparison)
+ */
+public class Team implements ReadOnlyTeam {
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Names should only contain alphanumeric characters and spaces, and it should not be blank";
+
+    /*
+     * The first character of the address must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+    private static final int MAX_CAPACITY = 5;
+
+    private final String name;
+    private final UniquePersonList persons;
+
+    /*
+     * The 'unusual' code block below is a non-static initialization block, sometimes used to avoid duplication
+     * between constructors. See https://docs.oracle.com/javase/tutorial/java/javaOO/initial.html
+     *
+     * Note that non-static init blocks are not recommended to use. There are other ways to avoid duplication
+     *   among constructors.
+     */
+    {
+        persons = new UniquePersonList();
+    }
+
+    /**
+     * Constructs a {@code Team}.
+     *
+     * @param name A valid name.
+     */
+    public Team(String name) {
+        requireNonNull(name);
+        checkArgument(isValidName(name), MESSAGE_CONSTRAINTS);
+        this.name = name;
+    }
+
+    /**
+     * Creates an Team using the Persons in the {@code toBeCopied}
+     */
+    public Team(String name, ReadOnlyTeam toBeCopied) {
+        requireNonNull(name);
+        checkArgument(isValidName(name), MESSAGE_CONSTRAINTS);
+        this.name = name;
+        resetData(toBeCopied);
+    }
+
+    //// list overwrite operations
+
+    /**
+     * Replaces the contents of the person list with {@code persons}.
+     * {@code persons} must not contain duplicate persons.
+     */
+    public void setPersons(List<Person> persons) {
+        if (persons.size() > MAX_CAPACITY) {
+            throw new TeamMaxCapacityException();
+        }
+        this.persons.setPersons(persons);
+    }
+
+    /**
+     * Resets the existing data of this {@code Team} with {@code newData}.
+     */
+    public void resetData(ReadOnlyTeam newData) {
+        requireNonNull(newData);
+
+        setPersons(newData.getPersonList());
+    }
+
+    //// person-level operations
+
+    /**
+     * Returns true if a person with the same identity as {@code person} exists in the team.
+     */
+    public boolean hasPerson(Person person) {
+        requireNonNull(person);
+        return persons.contains(person);
+    }
+
+    /**
+     * Adds a person to the team.
+     * The person must not already exist in the team.
+     */
+    public void addPerson(Person p) {
+        if (persons.size() + 1 > MAX_CAPACITY) {
+            throw new TeamMaxCapacityException();
+        }
+        persons.add(p);
+    }
+
+    /**
+     * Replaces the given person {@code target} in the list with {@code editedPerson}.
+     * {@code target} must exist in the team.
+     * The person identity of {@code editedPerson} must not be the same as another existing person in the team.
+     */
+    public void setPerson(Person target, Person editedPerson) {
+        requireNonNull(editedPerson);
+
+        persons.setPerson(target, editedPerson);
+    }
+
+    /**
+     * Removes {@code key} from this {@code Team}.
+     * {@code key} must exist in the team.
+     */
+    public void removePerson(Person key) {
+        persons.remove(key);
+    }
+
+    public String getName() {
+        return name;
+    }
+    //// util methods
+
+    /**
+     * Returns true if a given string is a valid name.
+     */
+    public static boolean isValidName(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("name", name)
+                .add("persons", persons)
+                .toString();
+    }
+
+    @Override
+    public ObservableList<Person> getPersonList() {
+        return persons.asUnmodifiableObservableList();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Team)) {
+            return false;
+        }
+
+        Team otherTeam = (Team) other;
+        return persons.equals(otherTeam.persons);
+    }
+
+    @Override
+    public int hashCode() {
+        return persons.hashCode();
+    }
+}
