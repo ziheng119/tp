399334935ab67b129773fbf506d3372401e2475e
ctrf03 <clintontayrf.ct@gmail.com>
2025-10-09 16:28:26 +0800

    Update Style on SampleDataUtil.java

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -12,30 +12,29 @@ import seedu.address.model.person.Phone;
  * Contains utility methods for populating {@code AddressBook} with sample data.
  */
 public class SampleDataUtil {
-        public static Person[] getSamplePersons() {
-                return new Person[] {new Person(new Name("Alex Yeoh"), new Phone("87438807"),
-                                new Email("alexyeoh@example.com"), new Github("AlexYeoh")),
-                                new Person(new Name("Bernice Yu"), new Phone("99272758"),
-                                                new Email("berniceyu@example.com"),
-                                                new Github("BerniceYu")),
-                                new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"),
-                                                new Email("charlotte@example.com"),
-                                                new Github("CharlotteO")),
-                                new Person(new Name("David Li"), new Phone("91031282"),
-                                                new Email("lidavid@example.com"),
-                                                new Github("DavidLi")),
-                                new Person(new Name("Irfan Ibrahim"), new Phone("92492021"),
-                                                new Email("irfan@example.com"),
-                                                new Github("IrfanIbrahim")),
-                                new Person(new Name("Roy Balakrishnan"), new Phone("92624417"),
-                                                new Email("royb@example.com"), new Github("RoyB"))};
-        }
 
-        public static ReadOnlyAddressBook getSampleAddressBook() {
-                AddressBook sampleAb = new AddressBook();
-                for (Person samplePerson : getSamplePersons()) {
-                        sampleAb.addPerson(samplePerson);
-                }
-                return sampleAb;
+    public static Person[] getSamplePersons() {
+        return new Person[] {
+            new Person(new Name("Alex Yeoh"), new Phone("87438807"),
+                    new Email("alexyeoh@example.com"), new Github("AlexYeoh")),
+            new Person(new Name("Bernice Yu"), new Phone("99272758"),
+                    new Email("berniceyu@example.com"), new Github("BerniceYu")),
+            new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"),
+                    new Email("charlotte@example.com"), new Github("CharlotteO")),
+            new Person(new Name("David Li"), new Phone("91031282"),
+                    new Email("lidavid@example.com"), new Github("DavidLi")),
+            new Person(new Name("Irfan Ibrahim"), new Phone("92492021"),
+                    new Email("irfan@example.com"), new Github("IrfanIbrahim")),
+            new Person(new Name("Roy Balakrishnan"), new Phone("92624417"),
+                    new Email("royb@example.com"), new Github("RoyB"))
+        };
+    }
+
+    public static ReadOnlyAddressBook getSampleAddressBook() {
+        AddressBook sampleAb = new AddressBook();
+        for (Person samplePerson : getSamplePersons()) {
+            sampleAb.addPerson(samplePerson);
         }
+        return sampleAb;
+    }
 }
