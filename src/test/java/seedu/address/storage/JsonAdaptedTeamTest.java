260de3f77225675354e7944391161762181b616e
Bacen <ywj.ethan@gmail.com>
2025-10-16 18:46:59 +0800

    Fix Checkstyle

--- src/test/java/seedu/address/storage/JsonAdaptedTeamTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedTeamTest.java
@@ -1,7 +1,6 @@
 package seedu.address.storage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import java.util.Arrays;
