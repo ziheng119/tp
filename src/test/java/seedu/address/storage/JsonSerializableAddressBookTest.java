81b244a4383b2709c4d22cc96a8113f85e17eada
lihloway <80195957+lihloway@users.noreply.github.com>
2025-10-22 19:34:42 +0800

    Debug testcase 4

--- src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
@@ -1,7 +1,6 @@
 package seedu.address.storage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import java.nio.file.Path;
@@ -29,7 +28,7 @@ public class JsonSerializableAddressBookTest {
                 JsonSerializableAddressBook.class).get();
         AddressBook addressBookFromFile = dataFromFile.toModelType();
         AddressBook typicalPersonsAddressBook = TypicalPersons.getTypicalAddressBook();
-        assertTrue(addressBookFromFile.equals(typicalPersonsAddressBook));
+        assertEquals(addressBookFromFile, typicalPersonsAddressBook);
     }
 
     @Test
