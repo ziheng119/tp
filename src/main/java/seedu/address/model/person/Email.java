100f19a219fca35ced58d0880023cc08d479c95b
Cavan <cavanchua0975@gmail.com>
2025-11-02 19:08:28 +0800

    Update email error message

--- src/main/java/seedu/address/model/person/Email.java
+++ src/main/java/seedu/address/model/person/Email.java
@@ -17,7 +17,7 @@ public class Email {
             + "and adhere to the following constraints:\n"
             + "1. The local-part should only contain alphanumeric characters and these special characters, excluding "
             + "the parentheses, (" + SPECIAL_CHARACTERS + "). The local-part may not start or end with any special "
-            + "characters.\n"
+            + "characters or contain consecutive special characters.\n"
             + "2. This is followed by a '@' and then a domain name. The domain name is made up of domain labels "
             + "separated by at least one period.\n"
             + "The domain name must:\n"
