7d5676f532ba68e050269b9c27c4a57ab0dde734
Bacen <ywj.ethan@gmail.com>
2025-11-02 21:40:35 +0800

    Fix format

--- src/test/java/seedu/address/logic/commands/RemoveFromTeamCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemoveFromTeamCommandTest.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BOB;
